1b133b6f7fadce325c682c7774634cfb8d171382
Grogwood LLC <dev@grogwood.com>
2020-11-04 17:45:21 -0800

    Added new AuthorizeNetConfig and AuthorizeNetAPIEnpoint classes

--- src/AuthorizeNetPorter.php
+++ src/AuthorizeNetPorter.php
@@ -3,6 +3,8 @@
    namespace Grayl\Omnipay\AuthorizeNet;
 
    use Grayl\Mixin\Common\Traits\StaticTrait;
+   use Grayl\Omnipay\AuthorizeNet\Config\AuthorizeNetAPIEndpoint;
+   use Grayl\Omnipay\AuthorizeNet\Config\AuthorizeNetConfig;
    use Grayl\Omnipay\AuthorizeNet\Controller\AuthorizeNetAuthorizeRequestController;
    use Grayl\Omnipay\AuthorizeNet\Controller\AuthorizeNetCaptureRequestController;
    use Grayl\Omnipay\AuthorizeNet\Entity\AuthorizeNetAuthorizeRequestData;
@@ -19,7 +21,7 @@
 
    /**
     * Front-end for the AuthorizeNet Omnipay package
-    * @method AuthorizeNetGatewayData getSavedGatewayDataEntity ( string $endpoint_id )
+    * @method AuthorizeNetGatewayData getSavedGatewayDataEntity ( string $api_endpoint_id )
     *
     * @package Grayl\Omnipay\AuthorizeNet
     */
@@ -34,18 +36,25 @@
        *
        * @var string
        */
-      protected string $config_file = 'gateway.omnipay.authorizenet.php';
+      protected string $config_file = 'omnipay-authorizenet.php';
+
+      /**
+       * The AuthorizeNetConfig instance for this gateway
+       *
+       * @var AuthorizeNetConfig
+       */
+      protected $config;
 
 
       /**
        * Creates a new Omnipay ApiGateway object for use in a AuthorizeNetGatewayData entity
        *
-       * @param array $credentials An array containing all of the credentials needed to create the gateway API
+       * @param AuthorizeNetAPIEndpoint $api_endpoint A AuthorizeNetAPIEndpoint with credentials needed to create a gateway API object
        *
        * @return ApiGateway
        * @throws \Exception
        */
-      public function newGatewayAPI ( array $credentials ): object
+      public function newGatewayAPI ( $api_endpoint ): object
       {
 
          // Create the Omnipay AuthorizeNetGatewayData api entity
@@ -53,8 +62,8 @@
          $api = Omnipay::create( 'AuthorizeNetApi_Api' );
 
          // Set the credentials into the API
-         $api->setAuthName( $credentials[ 'login_id' ] );
-         $api->setTransactionKey( $credentials[ 'transaction_key' ] );
+         $api->setAuthName( $api_endpoint->getLoginID() );
+         $api->setTransactionKey( $api_endpoint->getTransactionKey() );
 
          // Return the new instance
          return $api;
@@ -64,21 +73,21 @@
       /**
        * Creates a new AuthorizeNetGatewayData entity
        *
-       * @param string $endpoint_id The API endpoint ID to use (typically "default" is there is only one API gateway)
+       * @param string $api_endpoint_id The API endpoint ID to use (typically "default" if there is only one API gateway)
        *
        * @return AuthorizeNetGatewayData
        * @throws \Exception
        */
-      public function newGatewayDataEntity ( string $endpoint_id ): object
+      public function newGatewayDataEntity ( string $api_endpoint_id ): object
       {
 
          // Grab the gateway service
          $service = new AuthorizeNetGatewayService();
 
-         // Get an API
-         $api = $this->newGatewayAPI( $service->getAPICredentials( $this->config,
-                                                                   $this->environment,
-                                                                   $endpoint_id ) );
+         // Get a new API
+         $api = $this->newGatewayAPI( $service->getAPIEndpoint( $this->config,
+                                                                $this->environment,
+                                                                $api_endpoint_id ) );
 
          // Configure the API as needed using the service
          $service->configureAPI( $api,
@@ -86,7 +95,7 @@
 
          // Return the gateway
          return new AuthorizeNetGatewayData( $api,
-                                             $this->config->getConfig( 'name' ),
+                                             $this->config->getGatewayName(),
                                              $this->environment );
       }
 
